3c89e4e2e4ce1cb3abbb7f98096dd96dddda8670
Guy Rozendorn <guy@rzn.co.il>
2012-08-29 23:17:16 +0300

    TRIVIAL initial work

--- src/pypi_switcher/__init__.py
+++ src/pypi_switcher/__init__.py
@@ -1,1 +1,31 @@
 __import__("pkg_resources").declare_namespace(__name__)
+
+from sys import argv
+from infi.pyutils.contexts import contextmanager
+from os import path
+
+@contextmanager
+def open_configparser_file(filepath, write_on_exit=False):
+    from ConfigParser import ConfigParser
+    parser = ConfigParser()
+    if path.exists(filepath):
+        parser.read(filepath)
+    try:
+        yield parser
+    finally:
+        if not write_on_exit:
+            return
+        with open(filepath, 'w') as fd:
+            parser.write(fd)
+
+def set_index_url_in_file(filepath, section_name, key, index_url):
+    with open_configparser_file(path.expanduser(filepath), True) as pydistutils:
+        if not pydistutils.has_section(section_name):
+            pydistutils.add_section(section_name)
+        pydistutils.set(section_name, key, index_url)
+
+def set_index_url(argv=argv[1:]):
+    [index_url] = argv
+    set_index_url_in_file("~/.pydistutils.cfg", "easy_install", "index-url", index_url)
+    set_index_url_in_file("~/.buildout/default.cfg", "buildout", "index", index_url)
+
