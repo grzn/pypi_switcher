2b899fb294c5901632e16bbdfab6f5da4a893abb
Guy Rozendorn <guy@rzn.co.il>
2013-02-03 16:18:02 +0200

    TRIVIAL pylint

--- src/pypi_switcher/__init__.py
+++ src/pypi_switcher/__init__.py
@@ -1,6 +1,6 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
-from sys import argv
+import sys
 from infi.pyutils.contexts import contextmanager
 from os import path, makedirs
 
@@ -28,9 +28,9 @@ def set_index_url_in_file(filepath, section_name, key, index_url):
             pydistutils.add_section(section_name)
         pydistutils.set(section_name, key, index_url)
 
-def set_index_url(argv=argv[1:]):
+
+def set_index_url(argv=sys.argv[1:]):
     [index_url] = argv
     set_index_url_in_file("~/.pydistutils.cfg", "easy_install", "index-url", index_url)
     set_index_url_in_file("~/.pip/pip.conf", "global", "index-url", index_url)
     set_index_url_in_file("~/.buildout/default.cfg", "buildout", "index", index_url)
-
